11e453061ae3fd279d79fc2076933cdf4b7c61ca
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-08-16 14:51:19 +0800

    main function init logging

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 /*
  * Main event loop
  */
+#include "log.h"
 
 int main(int argc, char **argv) {
+    init_logger("stderr", INFO);
+    log_infof(__func__, "udp-tun started, version 0.1");
     // Create kernel tunnel
     
     // Waiting for the package to be processed
