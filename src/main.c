741e1ab1649a73ee5ad770076c81a235d709fb5c
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2017-08-16 20:51:37 +0800

    Fix segment fault

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 
 int main(int argc, char **argv) {
     int tun_fd = -1;
+    char tun_name[] = "udptun";
     init_logger("stderr", INFO);
     log_infof(__func__, "udp-tun started, version 0.1");
     // TODO: get options
 
     // Create kernel tunnel
-    tun_fd = tun_alloc("udptun");
-    log_infof(__func__, "tunnel create success, devname: %s", "udptun");
+    tun_fd = tun_alloc(tun_name);
+    log_infof(__func__, "tunnel create success, devname: %s", tun_name);
     
     // Waiting for the package to be processed
     // start the pacakge loop
